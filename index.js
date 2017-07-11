307cbc47ee68ad9d4bd25c2d3c79f208ea25652d
Jannis R <mail@jannisr.de>
2017-07-11 20:06:12 +0200

    WIP

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const widget = require('virtual-widget')
 const h = require('hyperscript')
-const on = require('add-event-listener')
+const {addEventListener: on, removeEventListener: off} = require('add-event-listener')
 
 const className = 'virtual-dom-autocomplete'
 
@@ -13,41 +13,74 @@ const defaults = {
 function Autocomplete (text, suggest, onChange, opt = {}) {
 	if (!(this instanceof Autocomplete)) return new Autocomplete(suggest, onChange, opt)
 
-	this.text = text
+	this.text = text || ''
+	this.placeholder = opt.placeholder || defaults.placeholder || ''
 	this.onChange = onChange
 	this.suggest = suggest
-	this.placeholder = opt.placeholder || defaults.placeholder
+
+	this.wrapperEl = null
+	this.inputEl = null
+	this.onKeypress = null
+	this.suggestionsEl = null
 }
-Autocomplete.prototype.type = 'Widget'
 
-Autocomplete.prototype.init = function init () {
-	this.el = h('input', {
+const p = Autocomplete.prototype
+p.type = 'Widget'
+
+p.init = function init () {
+	const inputEl = this.inputEl = h('input', {
+		class: className + '-input',
 		type: 'text',
-		placeholder: this.placeholder || '',
-		value: this.text || ''
+		placeholder: this.placeholder,
+		value: this.text
 	})
+	this.wrapperEl = h('div', {class: className}, inputEl)
 
-	const onKeypress = (e) => {
+	const self = this
+	this.onKeypress = (e) => {
 		if (e.altKey || e.ctrlKey || e.metaKey) return
-		const self = this
 		setTimeout(() => {
-			self.onChange.call({}, e.target.value)
+			if (e.target.value !== this.text) self.onChange.call({}, e.target.value)
 		}, 1)
 	}
-	on(this.el, 'keypress', onKeypress)
+	on(this.inputEl, 'keypress', this.onKeypress)
 
-	return this.el
+	this.updateSuggestions()
+	return this.wrapperEl
 }
 
-Autocomplete.prototype.update = function update (update) {
-	if (this.el !== update.el) {
-		this.el = update.el
-		this.text = update.text
-	} else if (this.el.value !== update.text) {
-		this.el.value = this.text = update.text
-		const suggestions = this.suggest.call({}, this.text)
-		// todo
+p.update = function update (old) {
+	console.log('applying to DOM', this.text, old.text, this.placeholder, old.placeholder)
+	if (!this.wrapperEl) this.init()
+
+	if (this.text !== old.text) { // text changed
+		this.inputEl.value = this.text
+		// this.updateSuggestions()
 	}
+	if (this.placeholder !== old.placeholder) { // placeholder changed
+		this.inputEl.setAttribute('placeholder', this.placeholder)
+	}
+
+	return this.wrapperEl
+}
+
+p.updateSuggestions = function updateSuggestions () {
+	const suggestions = this.suggest.call({}, this.text)
+	console.log('new suggestions', suggestions)
+	const rendered = h('div', {
+		class: className + '-suggestions'
+	}, suggestions.map(this.renderSuggestion))
+
+	if (this.suggestionsEl) this.wrapperEl.removeChild(this.suggestionsEl)
+	this.wrapperEl.appendChild(this.suggestionsEl = rendered)
+}
+
+p.renderSuggestion = function renderSuggestion (text) {
+	return null
+}
+
+p.destroy = function destroy () {
+	off(this.inputEl, 'keypress', this.onKeypress)
 }
 
 module.exports = Autocomplete
